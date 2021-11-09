feccc443e6386bc0aeaebdb2a06a1514a0f01807
NikolaiIDS <nikolaisimeonov60@gmail.com>
2021-11-09 22:58:17 +0200

    animsFixed

--- Magical_Forest/Assets/CharacterController/Sctripts/AnimationStateController.cs
+++ Magical_Forest/Assets/CharacterController/Sctripts/AnimationStateController.cs
@@ -28,6 +28,11 @@ public class AnimationStateController : MonoBehaviour
         bool isRunning = animator.GetBool(isRunningHash);
         bool isCrouched = Input.GetKeyDown(KeyCode.LeftControl);
         bool isCrouchWalking = animator.GetBool(isCrouchW);
+        
+        if ((!Input.GetKey(KeyCode.W) || !Input.GetKey(KeyCode.S) || !Input.GetKey(KeyCode.D) || !Input.GetKey(KeyCode.A)) && Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("isCWfromW", false);
+        }
         if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.SetBool("isJumping", true);
@@ -39,6 +44,14 @@ public class AnimationStateController : MonoBehaviour
         if (Input.GetKey(KeyCode.W)|| Input.GetKey(KeyCode.S)|| Input.GetKey(KeyCode.D)|| Input.GetKey(KeyCode.A) && !isWalking)
         {
             animator.SetBool(isWalkingHash, true);
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+              animator.SetBool("isCWfromW", true); 
+            }
+            if (!Input.GetKey(KeyCode.LeftControl))
+            {
+                animator.SetBool("isCWfromW", false);
+            }
         }
         if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A)))
         {
@@ -51,6 +64,14 @@ public class AnimationStateController : MonoBehaviour
         if (isRunning && (!shiftPressed || !(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))))
         {
             animator.SetBool(isRunningHash, false);
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                animator.SetBool("isCWfromW", true);
+            }
+            if (!Input.GetKey(KeyCode.LeftControl))
+            {
+                animator.SetBool("isCWfromW", false);
+            }
         }
         if (Input.GetKey(KeyCode.LeftControl))
         {
@@ -68,6 +89,15 @@ public class AnimationStateController : MonoBehaviour
         {
             animator.SetBool("IsCrouching", false);
         }
+        if (animator.GetBool("IsCrouching")==true && animator.GetBool("isCrouchW")==false)
+        {
+            animator.SetBool("fromCtoCI", true);
+        }
+        if (!animator.GetBool("IsCrouching") == true || animator.GetBool("isCrouchW") == true)
+        {
+            animator.SetBool("fromCtoCI", false);
+        }
+
 
     }
         
