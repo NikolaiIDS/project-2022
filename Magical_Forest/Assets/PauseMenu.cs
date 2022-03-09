30fd91c2f6aa4754ed5654b90a9cc80f9af1d879
NikolaiIDS <nikolaisimeonov60@gmail.com>
2022-03-09 21:40:49 +0200

    swords

--- Magical_Forest/Assets/PauseMenu.cs
+++ Magical_Forest/Assets/PauseMenu.cs
@@ -10,17 +10,26 @@ public class PauseMenu : MonoBehaviour
     public GameObject settings;
     public GameObject menu;
     bool sett = false;
-
+    public GameObject swordsMenu;
+    public GameObject sword1;
+    public GameObject sword2;
+    public GameObject sword3;
+    public GameObject sword4;
+    public GameObject staff;
+    bool holdings;
+    bool holding1;
+    bool holding2;
+    bool holding3;
+    bool holding4;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (Paused && sett == false)
             {
-
                 Resume();
             }
-            else 
+            else
             {
                 Pause();
 
@@ -30,9 +39,15 @@ public class PauseMenu : MonoBehaviour
         {
             settings.SetActive(false); menu.SetActive(true); sett = false;
         }
+        if (Paused == false && Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwordMenu();
+        }
+        else if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            SwordMenunt();
+        }
        
-
-
     }
     public void Resume()
     {
@@ -53,6 +68,74 @@ public class PauseMenu : MonoBehaviour
             Paused = true;
         }
     }
+    public void SwordMenu()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+        swordsMenu.SetActive(true);
+    }
+    public void Item1()
+    {
+        if ( holding2 || holding3 || holding4)
+        {
+            
+            sword2.SetActive(false);
+            sword3.SetActive(false);
+            sword4.SetActive(false);
+        }
+        sword1.SetActive(true);
+        staff.SetActive(false);
+        holding1 = true;
+    }
+    public void Item2()
+    {
+        if (holding1 || holding3 || holding4)
+        {
+            sword1.SetActive(false);
+           
+            sword3.SetActive(false);
+            sword4.SetActive(false);
+        }
+        sword2.SetActive(true);
+        staff.SetActive(false);
+        holding2 = true;
+    }
+    public void Item3()
+    {
+        if (holding1 || holding2 || holding4)
+        {
+            sword1.SetActive(false);
+            sword2.SetActive(false);
+            sword3.SetActive(false);
+            sword4.SetActive(false);
+        }
+        sword3.SetActive(true);
+        staff.SetActive(false);
+        holding3 = true;
+    }
+    public void Item4()
+    {
+        if (holding1 || holding2 || holding3 )
+        {
+            sword1.SetActive(false);
+            sword2.SetActive(false);
+            sword3.SetActive(false);
+           
+        }
+        sword4.SetActive(true);
+        staff.SetActive(false);
+        holding4 = true;
+    }
+
+    public void SwordMenunt()
+    {
+        swordsMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
     public void QuitGame()
     {
         Application.Quit();
