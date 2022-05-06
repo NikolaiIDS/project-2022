45e076ce07b544b56dd2e1b6152f11a60d74b966
iliyancherganski <79467383+iliyancherganski@users.noreply.github.com>
2022-05-07 00:11:19 +0300

    pushhh

--- Magical_Forest/Assets/Prefabs/Structures/EnemyTowerScript.cs
+++ Magical_Forest/Assets/Prefabs/Structures/EnemyTowerScript.cs
@@ -4,7 +4,45 @@ using UnityEngine;
 
 public class EnemyTowerScript : MonoBehaviour
 {
+    public int shieldsHealth = 600;
+    public int maxShieldsHealth = 600;
 
+    public int coreHealth;
+    public int maxCoreHealth;
+
+    public GameObject[] shileds = new GameObject[3];
+    public bool[] _shieldExists = new bool[3] {true,true,true};
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Fireball"))
+        {
+            if (shieldsHealth > 0)
+            {
+                shieldsHealth -= collision.gameObject.GetComponent<FireballSccript>().dmg;
+                if (shieldsHealth <= 400 && _shieldExists[0])
+                {
+                    shileds[0].SetActive(false);
+                    _shieldExists[0] = false;                
+                }
+                else if (shieldsHealth <= 200 && _shieldExists[1])
+                {
+                    shileds[1].SetActive(false);
+                    _shieldExists[1] = false;
+                }
+                else if (shieldsHealth <= 0 && _shieldExists[2])
+                {
+                    shileds[2].SetActive(false);
+                    _shieldExists[2] = false;
+                }
+
+            }
+            else
+            {
+                maxCoreHealth -= collision.gameObject.GetComponent<FireballSccript>().dmg;
+            }
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +53,7 @@ public class EnemyTowerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         
     }
 }
